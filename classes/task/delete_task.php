f544b536cb8a4b4d6ea51e148964794c65f1d09d
Marcus Green <marcusavgreen@gmail.com>
2017-10-23 15:07:02 +0100

    code to delete assignments, not tested yet

--- classes/task/delete_task.php
+++ classes/task/delete_task.php
@@ -14,29 +14,52 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 //This can be tested on the command line with 
-
- //php admin/tool/task/cli/schedule_task.php --execute=\\block_homework\\task\\delete_task --showsql --showdebugging
-
+//php admin/tool/task/cli/schedule_task.php --execute=\\block_homework\\task\\delete_task --showsql --showdebugging
 namespace block_homework\task;
-
 /**
  * Scheduled task to delete assignments a set number of days after due date
  * @package    block_homework
  * @copyright  2017 Titus Learning by Marcus Green (@link http://www.tituslearning.com)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
 defined('MOODLE_INTERNAL') || die();
 
 require_once($CFG->dirroot . '/blocks/homework/edulink_classes/homework.php');
 
 class delete_task extends \core\task\scheduled_task {
+
     public function get_name() {
         return get_string('delete', 'block_homework');
     }
 
     public function execute() {
         mtrace('homework_block_delete_task start');
-        mtrace('homework_block_delete_task end');
+        /** find all courses that include the homework block */
+        global $DB;
+        $sql = "select ctx.instanceid as courseid from {context} ctx 
+            where contextlevel=50 and ctx.id in 
+            (select parentcontextid from {block_instances} b 
+            where blockname='homework')";
+        $courses = $DB->get_records_sql($sql);
+        /* 60 seconds in a minute, 60 minutes in an hour 24 hours in a day */
+        $deleteafterdays = get_config('block_homework')->deleteafterdays;
+        $daysagostamp = time() - ((60 * 60 * 24) * $deleteafterdays);
+        foreach ($courses as $course) {
+            $assigns = get_fast_modinfo($course->courseid)->get_instances_of('assign');
+            foreach ($assigns as $assign) {
+                $sql = "select duedate from {assign} where id =?";
+                $deadline = $DB->get_record_sql($sql, array($assign->instance));
+                /* 60 seconds in a minute, 60 minutes in an hour 24 hours in a day */
+                $daysagostamp = time() - ((60 * 60 * 24) * $deleteafterdays);
+                /* if the duedate is more than x days ago then delete assignment */
+                if ($deadline->duedate > $daysagostamp) {
+               // if(true){
+                    mtrace('Deleting assignment:' . $assign->id);
+                    course_delete_module($assign->id);
+                }
+            }
+            mtrace('homework_block_delete_task end');
+        }
     }
 }
+    
